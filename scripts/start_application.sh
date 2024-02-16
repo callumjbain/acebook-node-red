073456b5fd4fa8c7e1ef126f44163c5062f2b96c
nekocho <zoewycho@gmail.com>
2024-02-16 16:47:38 +0000

    Changed sh files

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,7 +1,9 @@
+#!/bin/bash
 cd /var/www/html/
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 nvm install node
 
-npm install
+npm install pm2 -g
+pm2 startOrReload ecosystem.config.json
